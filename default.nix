e46b57123febbad65b4a41c12b0124459ec8ff4f
akhaten <khafifjessy.github@gmail.com>
2023-02-22 12:27:12 +0100

    default.nix updated

--- default.nix
+++ default.nix
@@ -2,9 +2,29 @@
 {system ? builtins.currentSystem} :
 
 let pkgs = import <nixpkgs> { inherit system; };
-in pkgs.stdenv.mkDerivation {
-    Ut3 = pkgs.python3Packages.buildPythonPackage {
-        name = "Ut3";
-        src = ./.;
-    };
+# in pkgs.stdenv.mkDerivation {
+# 	name = "CelcatUT3";
+#     Ut3 = pkgs.python310Packages.buildPythonPackage {
+#         name = "Ut3";
+#         src = ./.;
+# 		propagatedBuildInputs = with pkgs; [
+# 			python310
+# 			python310Packages.requests
+# 			python310Packages.python-dateutil
+# 			python310Packages.parse
+# 		];
+# 		doCheck = false;
+#     };
+# }
+
+in pkgs.python310Packages.buildPythonPackage {
+    name = "Ut3";
+    src = ./.;
+	propagatedBuildInputs = with pkgs; [
+		python310
+		python310Packages.requests
+		python310Packages.python-dateutil
+		python310Packages.parse
+	];
+	doCheck = false;
 }
